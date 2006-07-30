9917cb34dcce88ef4c38a3451c35a818c0f74975
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2006-07-30 15:59:41 +0000

    Added validations.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk/rhino-commons@86 079b0acf-d9fa-0310-9935-e5ade295c882

--- Rhino.Commons/Validation.cs
+++ Rhino.Commons/Validation.cs
@@ -13,6 +13,13 @@ namespace Rhino.Commons
                 throw new ArgumentException("The start date cannot come after the end date");
             }
         }
+
+        public static void NotNullOrEmpty(string str, string name)
+        {
+            if (string.IsNullOrEmpty(str))
+                throw new ArgumentException("{0} must have a value", name);
+        }
+
         
         public static void NotNull(object obj, string paramName)
         {
@@ -24,6 +31,26 @@ namespace Rhino.Commons
         {
             if (start.CompareTo(obj) > 0 || end.CompareTo(obj) < 0)
                 throw new ArgumentOutOfRangeException(paramName);
+        }
+
+        public static void InDateRange(
+         DateTime innerStart, DateTime innerEnd,
+         DateTime outerStart, DateTime outerEnd)
+        {
+            if (innerStart < outerStart || innerStart > outerEnd ||
+                innerEnd > outerEnd || innerEnd < outerStart)
+                throw
+                    new ArgumentOutOfRangeException(
+                        string.Format("Date Ranges do not overlap, {0}-{1} does not contain {2}-{3}",
+                                      outerStart.ToShortDateString(), outerEnd.ToShortDateString(),
+                                      innerStart.ToShortDateString(), innerEnd.ToShortDateString()));
+        }
+
+        public static void PositiveNumber(int number, string name)
+        {
+            if (number < 0)
+                throw new ArgumentException(
+                    string.Format("{1} should be positive, but was {0}", number, name));
         }
     }
 }
