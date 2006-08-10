5207fc971bb39702b2019a1c94fb19268ab05958
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2006-08-10 07:20:33 +0000

    Small nesting bug.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@93 079b0acf-d9fa-0310-9935-e5ade295c882

--- rhino-commons/Rhino.Commons/UnitOfWork/NHibernateUnitOfWorkAdapter.cs
+++ rhino-commons/Rhino.Commons/UnitOfWork/NHibernateUnitOfWorkAdapter.cs
@@ -6,47 +6,61 @@ using NHibernate;
 
 namespace Rhino.Commons
 {
-    public class NHibernateUnitOfWorkAdapter : IUnitOfWork
-    {
-        private ISession session;
+	public class NHibernateUnitOfWorkAdapter : IUnitOfWork
+	{
+		private ISession session;
 
 		private readonly NHibernateUnitOfWorkAdapter previous;
+		private int usageCount = 1;
 
 		public NHibernateUnitOfWorkAdapter Previous
-    	{
-    		get { return previous; }
-    	}
+		{
+			get { return previous; }
+		}
 
-    	public ISession Session
-        {
-            get { return session; }
-        }
+		public ISession Session
+		{
+			get { return session; }
+		}
 
-        public void Flush()
-        {
-            session.Flush();
-        }
+		public void Flush()
+		{
+			session.Flush();
+		}
 
-        public ITransaction BeginTransaction()
-        {
-            return session.BeginTransaction();
-        }
+		public ITransaction BeginTransaction()
+		{
+			return session.BeginTransaction();
+		}
 
-        public ITransaction BeginTransaction(IsolationLevel isolationLevel)
-        {
-            return session.BeginTransaction(isolationLevel);
-        }
+		public ITransaction BeginTransaction(IsolationLevel isolationLevel)
+		{
+			return session.BeginTransaction(isolationLevel);
+		}
 
-        public void Dispose()
-        {
+		public void Dispose()
+		{
+			usageCount -= 1;
+			if (usageCount != 0)
+				return;
 			UnitOfWork.DisposeUnitOfWork(this);
 			session.Dispose();
-        }
+		}
 
 		public NHibernateUnitOfWorkAdapter(ISession session, NHibernateUnitOfWorkAdapter previous)
-        {
-            this.session = session;
-        	this.previous = previous;
-        }
-    }
+		{
+			this.session = session;
+			this.previous = previous;
+		}
+
+		/// <summary>
+		/// Add another usage to this.
+		/// Will increase the dispose count
+		/// NOT THREAD SAFE
+		/// </summary>
+		public void IncremementUsages()
+		{
+			usageCount += 1;
+		}
+	}
 }
