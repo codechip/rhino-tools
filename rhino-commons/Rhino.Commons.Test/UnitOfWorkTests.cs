f2cd51860a5c787f22b1f96632573396d1049ed3
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2006-08-10 07:15:39 +0000

    Added nesting options.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@92 079b0acf-d9fa-0310-9935-e5ade295c882

---
+++ rhino-commons/Rhino.Commons.Test/UnitOfWorkTests.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Rhino.Commons.Test
+{
+	[TestFixture]
+	public class UnitOfWorkTests
+	{
+		[Test]
+		public void CanNestUnitOfWork()
+		{
+			IUnitOfWork first = UnitOfWork.Start();
+			Assert.AreEqual(first, UnitOfWork.Current);
+			Assert.AreEqual(((NHibernateUnitOfWorkAdapter)first).Session,
+			                UnitOfWork.CurrentNHibernateSession);
+			
+			IUnitOfWork second = UnitOfWork.Start(UnitOfWorkNestingOptions.CreateNewOrNestUnitOfWork);
+			Assert.AreEqual(second, UnitOfWork.Current);
+			Assert.AreEqual(((NHibernateUnitOfWorkAdapter)second).Session,
+			                UnitOfWork.CurrentNHibernateSession);
+			
+			second.Dispose();
+			Assert.AreEqual(first, UnitOfWork.Current);
+			Assert.AreEqual(((NHibernateUnitOfWorkAdapter)first).Session,
+							UnitOfWork.CurrentNHibernateSession);
+			first.Dispose();
+			Assert.IsNull(Local.Data[UnitOfWork.CurrentNHibernateSessionKey]);
+			
+		}
+	}
+}
