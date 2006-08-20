56d8a6925022a7035e2509eb8d4271fbcdced99c
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2006-08-20 22:06:19 +0000

    Removed configuration attribute support and nested configuration facility. Using custom castle's version. Added initial support for objectInstanceResolver.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk/rhino-commons@100 079b0acf-d9fa-0310-9935-e5ade295c882

---
+++ Rhino.Commons.Test/IoC/IoCFitxture.cs
@@ -0,0 +1,30 @@
+using Castle.Windsor;
+using NUnit.Framework;
+
+namespace Rhino.Commons.Test.IoCTests
+{
+    [TestFixture]
+    public class IoCFitxture
+    {
+        [TearDown]
+        public void TestCleanup()
+        {
+            IoC.Initialize(null);
+        }
+        
+        [Test]
+        public void LocalContainerOverideGlobalOne()
+        {
+            WindsorContainer container = new WindsorContainer();
+            IoC.Initialize(container);
+            Assert.AreSame(container,IoC.Container);
+            WindsorContainer localContainer = new WindsorContainer();
+            using(IoC.UseLocalContainer(localContainer))
+            {
+                Assert.AreSame(localContainer, IoC.Container);
+            }
+            Assert.AreSame(container, IoC.Container);
+        }
+    }
+}
+    
--- Rhino.Commons/RhinoContainer/RhinoContainer.cs
+++ Rhino.Commons/RhinoContainer/RhinoContainer.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Reflection;
 using Castle.MicroKernel;
+using Castle.MicroKernel.Handlers;
 using Castle.MicroKernel.SubSystems.Conversion;
+using Castle.Model;
 using Castle.Windsor;
 using Castle.Windsor.Configuration;
 using Castle.Windsor.Configuration.Interpreters;
@@ -8,6 +12,13 @@ namespace Rhino.Commons
 {
     public class RhinoContainer : WindsorContainer
     {
+        private ObjectInstanceResolver objectInstanceResolver;
+
+        public RhinoContainer()
+        {
+            Initializer();
+        }
+        
         public RhinoContainer(string fileName) 
             : this(new XmlInterpreter(fileName))
         {
@@ -15,11 +26,60 @@ namespace Rhino.Commons
 
         public RhinoContainer(IConfigurationInterpreter interpreter) : base()
         {
-            DefaultConversionManager conversionManager = (DefaultConversionManager) Kernel.GetSubSystem(SubSystemConstants.ConversionManagerKey);
-            conversionManager.Add(new ConfigurationObjectConverter());
-         
+            Initializer();
             interpreter.ProcessResource(interpreter.Source, Kernel.ConfigurationStore);
             RunInstaller();
+        }
+
+        private void Initializer()
+        {
+            objectInstanceResolver = new ObjectInstanceResolver();
+            Kernel.Resolver.AddSubResolver(objectInstanceResolver);
+        }
+        
+        public void RegisterDependencyItem(IHandler handler, string dependencyKey,
+            object instance)
+        {
+            Validation.NotNull(handler, "handler");
+            Validation.NotNull(dependencyKey, "dependencyKey");
+            Validation.NotNull(instance, "instance");
+            ComponentModel model = handler.ComponentModel;
+            foreach (ConstructorCandidate constructor in model.Constructors)
+            {
+                foreach (DependencyModel dependency in constructor.Dependencies)
+                {
+                    if (TryAddDependencyInstnace(handler, dependency, dependencyKey, instance))
+                        return;
+                }
+            }
+            foreach (DependencyModel dependency in model.Dependencies)
+            {
+                if (TryAddDependencyInstnace(handler, dependency, dependencyKey, instance))
+                    return;
+            }
+            throw new InvalidOperationException(string.Format("Could not find dependency {0} on model {1}", dependencyKey, model.Name));
+        }
+
+        private bool TryAddDependencyInstnace(IHandler handler, DependencyModel dependency, string dependencyKey, object instance)
+        {
+            if(dependency.DependencyKey == dependencyKey)
+            {
+                objectInstanceResolver.AddDependencyInstance(dependency, instance);
+                RecalculateHandlerDependencies(handler,dependency);
+                return true;
+            }
+            return false;
+        }
+
+        private void RecalculateHandlerDependencies(IHandler handler,DependencyModel dependency)
+        {
+            AbstractHandler abstractHandler = handler as AbstractHandler;
+            if(abstractHandler==null)
+            {
+                throw new InvalidOperationException(string.Format("Custom handler {0} does not inherit from abstract handler. Can't remove dependency from handler!", handler.GetType()));
+            }
+			abstractHandler.RemovedDependency(dependency);
+            
         }
     }
 }
