b0888081e637a98bb77298f1eabfea1dd0e557c1
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2006-09-07 08:14:23 +0000

    Fixing smart-ass implementation of local data. Now can also handle thread pool in web scenario.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@104 079b0acf-d9fa-0310-9935-e5ade295c882

--- rhino-commons/Rhino.Commons/LocalDataImpl/LocalData.cs
+++ rhino-commons/Rhino.Commons/LocalDataImpl/LocalData.cs
@@ -1,13 +1,52 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Web;
 using Rhino.Commons.LocalDataImpl;
 
 namespace Rhino.Commons
 {
     public class Local
     {
-        static ILocalData current = new LocalDataSwitcher(delegate(ILocalData real) { current = real; } );
+        static ILocalData current = new LocalData();
+        static object LocalDataHashtableKey = new object();
+        private class LocalData : ILocalData
+        {
+            [ThreadStatic]
+            static Hashtable thread_hashtable;
+
+            private static Hashtable Local_Hashtable
+            {
+                get
+                {
+                    if(HttpContext.Current==null)
+                    {
+                        return thread_hashtable ??
+                        (
+                            thread_hashtable = new Hashtable()
+                        );
+                    }
+                    Hashtable web_hashtable = HttpContext.Current.Items[LocalDataHashtableKey] as Hashtable;
+                    if(web_hashtable==null)
+                    {
+                        HttpContext.Current.Items[LocalDataHashtableKey] = web_hashtable = new Hashtable();
+                    }
+                    return web_hashtable;
+                }
+            }
+
+            public object this[object key]
+            {
+                get { return Local_Hashtable[key]; }
+                set { Local_Hashtable[key] = value; }
+            }
+
+            public void Clear()
+            {
+                Local_Hashtable.Clear();
+            }
+        }
 
         public static ILocalData Data
         {
