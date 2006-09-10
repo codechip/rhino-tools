762f9fc53f58f1d199e8afcd9acecc6fed762e45
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2006-09-10 12:18:58 +0000

    Many updates to correctly support instance per HttpApplication.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@106 079b0acf-d9fa-0310-9935-e5ade295c882

--- rhino-commons/Rhino.Commons/HttpModules/UnitOfWorkModule.cs
+++ rhino-commons/Rhino.Commons/HttpModules/UnitOfWorkModule.cs
@@ -1,34 +1,55 @@
 using System;
+using System.Runtime.CompilerServices;
 using System.Web;
 using Castle.Windsor;
+using log4net;
 using Rhino.Commons.Properties;
 
 namespace Rhino.Commons.HttpModules
 {
     public class UnitOfWorkModule : IHttpModule
     {
+        static ILog logger = LogManager.GetLogger(typeof (UnitOfWorkModule));
         private IWindsorContainer windsorContainer;
+        HttpApplication application;
 
         public void Init(HttpApplication context)
         {
-            windsorContainer = new RhinoContainer(Settings.Default.WindsorConfig);
-            IoC.Initialize(windsorContainer);
+            application = context;
+            logger.Info("Starting Unit Of Work Module");
+            InitializeContainer();
             context.BeginRequest += new EventHandler(context_BeginRequest);
             context.EndRequest += new EventHandler(context_EndRequest);
         }
 
-        void context_EndRequest(object sender, EventArgs e)
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        private void InitializeContainer()
         {
-            UnitOfWork.Current.Dispose();
+            if(IoC.IsInitialized)
+                return;
+            windsorContainer = new RhinoContainer(Settings.Default.WindsorConfig);
+            IoC.Initialize(windsorContainer);
         }
 
-        void context_BeginRequest(object sender, EventArgs e)
+        private void context_BeginRequest(object sender, EventArgs e)
         {
-            IUnitOfWork start = UnitOfWork.Start();
+            if(IoC.IsInitialized==false)
+                InitializeContainer();
+            logger.Debug("Starting Unit Of Work For Request");
+            UnitOfWork.Start();
+        }
+
+        void context_EndRequest(object sender, EventArgs e)
+        {
+            logger.Debug("Disposing Unit Of Work For Request");
+            UnitOfWork.Current.Dispose();
         }
 
         public void Dispose()
         {
+            application.BeginRequest -= new EventHandler(context_BeginRequest);
+            application.EndRequest -= new EventHandler(context_EndRequest);
+            logger.Info("Disposing Unit Of Work Module");
             IoC.Reset(windsorContainer);            
             windsorContainer.Dispose();
         }
--- rhino-commons/Rhino.Commons/RhinoContainer/IoC.cs
+++ rhino-commons/Rhino.Commons/RhinoContainer/IoC.cs
@@ -1,18 +1,20 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Web;
 using Castle.Windsor;
 
 namespace Rhino.Commons
 {
     public static class IoC
     {
+        private static IDictionary<IWindsorContainer, HttpApplicationState> container2Application = new Dictionary<IWindsorContainer, HttpApplicationState>();
         private static IWindsorContainer container;
         private static object LocalContainerKey = new object();
+        private static string GlobalContainerKey = "Global.Container.Key";
 
         public static void Initialize(IWindsorContainer windsorContainer)
         {
-            IoC.container = windsorContainer;
+            GlobalContainer = windsorContainer;
         }
 
         public static T Resolve<T>()
@@ -29,7 +31,7 @@ namespace Rhino.Commons
         {
             get
             {
-                IWindsorContainer result = LocalContainer ?? container;
+                IWindsorContainer result = LocalContainer ?? GlobalContainer;
                 if (result == null)
                     throw new InvalidOperationException("The container has not been initialized!");
                 return result;
@@ -40,6 +42,35 @@ namespace Rhino.Commons
         {
             get { return Local.Data[LocalContainerKey] as IWindsorContainer; }
             set { Local.Data[LocalContainerKey] = value; }
+        }
+
+        public static bool IsInitialized
+        {
+            get { return GlobalContainer != null; }
+        }
+
+        internal static IWindsorContainer GlobalContainer
+        {
+            get
+            {
+                if (HttpContext.Current != null)
+                {
+                    return HttpContext.Current.Application[GlobalContainerKey] as IWindsorContainer;
+                }
+                return container;
+            }
+            set
+            {
+                if (HttpContext.Current != null)
+                {
+                    lock (container2Application)
+                    {
+                        container2Application[value] = HttpContext.Current.Application;
+                        HttpContext.Current.Application[GlobalContainerKey] = value;
+                    }
+                }
+                container = value;
+            }
         }
 
         /// <summary>
@@ -60,9 +91,17 @@ namespace Rhino.Commons
 
         public static void Reset(IWindsorContainer containerToReset)
         {
-            if (ReferenceEquals(container, containerToReset))
+            lock (container2Application)
             {
-                container = null;
+                if (container2Application.ContainsKey(containerToReset))
+                {
+                    container2Application[containerToReset].Remove(GlobalContainerKey);
+                }
+                container2Application.Remove(containerToReset);
+            }
+            if (ReferenceEquals(GlobalContainer, containerToReset))
+            {
+                GlobalContainer = null;
             }
         }
     }
