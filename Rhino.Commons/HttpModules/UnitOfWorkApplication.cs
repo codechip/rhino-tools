d979829d1672cdc5db70d4f3e65b0f768b29cd28
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2007-01-08 22:04:01 +0000

    Will keep trying to re-load container if not initialized, helps with fixing configuration issues. Will automatically open/dispose unit of work on request.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk/rhino-commons@431 079b0acf-d9fa-0310-9935-e5ade295c882

--- Rhino.Commons/HttpModules/UnitOfWorkApplication.cs
+++ Rhino.Commons/HttpModules/UnitOfWorkApplication.cs
@@ -13,6 +13,26 @@ namespace Rhino.Commons.HttpModules
         static ILog logger = LogManager.GetLogger(typeof (UnitOfWorkApplication));
         private static IWindsorContainer windsorContainer;
 
+		public UnitOfWorkApplication()
+		{
+			BeginRequest += new EventHandler(UnitOfWorkApplication_BeginRequest);
+			EndRequest += new EventHandler(UnitOfWorkApplication_EndRequest);
+		}
+
+    	public virtual void UnitOfWorkApplication_BeginRequest(object sender, EventArgs e)
+		{
+			if (IoC.IsInitialized == false)
+				InitializeContainer();
+    		UnitOfWork.Start();
+		}
+
+    	public virtual void UnitOfWorkApplication_EndRequest(object sender, EventArgs e)
+    	{
+    		IUnitOfWork unitOfWork = UnitOfWork.Current;
+			if (unitOfWork!=null)
+    			unitOfWork.Dispose();
+    	}
+
     	public IWindsorContainer Container
     	{
 			get { return windsorContainer; }
@@ -20,18 +40,26 @@ namespace Rhino.Commons.HttpModules
 
 		public virtual void Application_Start(object sender, EventArgs e)
 		{
-			string windsorConfig = Settings.Default.WindsorConfig;
-			if (!Path.IsPathRooted(windsorConfig))
-			{
-				//In ASP.Net apps, the current directory and the base path are NOT the same.
-				windsorConfig = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, windsorConfig);
-			}
-			windsorContainer = new RhinoContainer(windsorConfig);
-			IoC.Initialize(windsorContainer);
+			InitializeContainer();
 		}
 
+		[MethodImpl(MethodImplOptions.Synchronized)]
+    	private static void InitializeContainer()
+    	{
+			if(IoC.IsInitialized)
+				return;
+    		string windsorConfig = Settings.Default.WindsorConfig;
+    		if (!Path.IsPathRooted(windsorConfig))
+    		{
+    			//In ASP.Net apps, the current directory and the base path are NOT the same.
+    			windsorConfig = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, windsorConfig);
+    		}
+    		windsorContainer = new RhinoContainer(windsorConfig);
+    		IoC.Initialize(windsorContainer);
+    	}
 
-		public virtual void Application_End(object sender, EventArgs e)
+
+    	public virtual void Application_End(object sender, EventArgs e)
 		{
 			if (windsorContainer != null)//can happen if this isn't the first app
 			{
