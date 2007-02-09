910b524a8be5cf9c03bb6a2cde2a9c72dae3a546
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2007-02-09 13:41:46 +0000

    Minor formatting fix
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk/rhino-commons@468 079b0acf-d9fa-0310-9935-e5ade295c882

--- Rhino.Commons/HttpModules/UnitOfWorkApplication.cs
+++ Rhino.Commons/HttpModules/UnitOfWorkApplication.cs
@@ -8,9 +8,9 @@ using Rhino.Commons.Properties;
 
 namespace Rhino.Commons.HttpModules
 {
-    public class UnitOfWorkApplication : HttpApplication, IContainerAccessor
-    {
-        private static IWindsorContainer windsorContainer;
+	public class UnitOfWorkApplication : HttpApplication, IContainerAccessor
+	{
+		private static IWindsorContainer windsorContainer;
 
 		public UnitOfWorkApplication()
 		{
@@ -18,24 +18,24 @@ namespace Rhino.Commons.HttpModules
 			EndRequest += new EventHandler(UnitOfWorkApplication_EndRequest);
 		}
 
-    	public virtual void UnitOfWorkApplication_BeginRequest(object sender, EventArgs e)
+		public virtual void UnitOfWorkApplication_BeginRequest(object sender, EventArgs e)
 		{
 			if (IoC.IsInitialized == false)
 				InitializeContainer(this);
-    		UnitOfWork.Start();
+			UnitOfWork.Start();
 		}
 
-    	public virtual void UnitOfWorkApplication_EndRequest(object sender, EventArgs e)
-    	{
-    		IUnitOfWork unitOfWork = UnitOfWork.Current;
-			if (unitOfWork!=null)
-    			unitOfWork.Dispose();
-    	}
+		public virtual void UnitOfWorkApplication_EndRequest(object sender, EventArgs e)
+		{
+			IUnitOfWork unitOfWork = UnitOfWork.Current;
+			if (unitOfWork != null)
+				unitOfWork.Dispose();
+		}
 
-    	public IWindsorContainer Container
-    	{
+		public IWindsorContainer Container
+		{
 			get { return windsorContainer; }
-    	}
+		}
 
 		public virtual void Application_Start(object sender, EventArgs e)
 		{
@@ -43,17 +43,17 @@ namespace Rhino.Commons.HttpModules
 		}
 
 		[MethodImpl(MethodImplOptions.Synchronized)]
-    	private static void InitializeContainer(UnitOfWorkApplication self)
-    	{
-			if(IoC.IsInitialized)
+		private static void InitializeContainer(UnitOfWorkApplication self)
+		{
+			if (IoC.IsInitialized)
 				return;
 			self.CreateContainer();
-    	}
+		}
 
 
-    	public virtual void Application_End(object sender, EventArgs e)
+		public virtual void Application_End(object sender, EventArgs e)
 		{
-			if (windsorContainer != null)//can happen if this isn't the first app
+			if (windsorContainer != null) //can happen if this isn't the first app
 			{
 				IoC.Reset(windsorContainer);
 				windsorContainer.Dispose();
@@ -61,14 +61,14 @@ namespace Rhino.Commons.HttpModules
 		}
 
 
-        public override void Dispose()
-        {
-            BeginRequest -= new EventHandler(UnitOfWorkApplication_BeginRequest);
-            EndRequest -= new EventHandler(UnitOfWorkApplication_EndRequest);
-        }
+		public override void Dispose()
+		{
+			BeginRequest -= new EventHandler(UnitOfWorkApplication_BeginRequest);
+			EndRequest -= new EventHandler(UnitOfWorkApplication_EndRequest);
+		}
 
-    	protected virtual void CreateContainer()
-    	{
+		protected virtual void CreateContainer()
+		{
 			string windsorConfig = Settings.Default.WindsorConfig;
 			if (!Path.IsPathRooted(windsorConfig))
 			{
@@ -77,6 +77,6 @@ namespace Rhino.Commons.HttpModules
 			}
 			windsorContainer = new RhinoContainer(windsorConfig);
 			IoC.Initialize(windsorContainer);
-    	}
-    }
+		}
+	}
 }
