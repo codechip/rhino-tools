ff001f01053b54c3b3919419b325b6217e72608b
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2007-06-25 08:16:43 +0000

    Changing controller life cycle to be per web request.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@676 079b0acf-d9fa-0310-9935-e5ade295c882

--- rhino-igloo/Rhino.Igloo/Web/BaseController.cs
+++ rhino-igloo/Rhino.Igloo/Web/BaseController.cs
@@ -42,7 +42,7 @@ namespace Rhino.Igloo
 	/// <summary>
 	/// Base class for all the controllers
 	/// </summary>
-	[Transient]
+	[PerWebRequest]
 	public class BaseController
 	{
 		private static IValidatorRegistry validationRegistry = new CachedValidationRegistry();
