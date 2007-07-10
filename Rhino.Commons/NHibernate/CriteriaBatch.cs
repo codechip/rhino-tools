153b6c9c3703c7630c7be4c6c8a4a9d06f614fa6
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2007-07-10 06:08:11 +0000

    Making CriteriaBatch more extendable and allowing detached work.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk/rhino-commons@704 079b0acf-d9fa-0310-9935-e5ade295c882

--- Rhino.Commons/NHibernate/CriteriaBatch.cs
+++ Rhino.Commons/NHibernate/CriteriaBatch.cs
@@ -8,18 +8,21 @@ namespace Rhino.Commons
 {
     public class CriteriaBatch
     {
+        private readonly ISession session;
         private DetachedCriteria currentCriteria;
-        private int currentIndex=-1;
+        private int currentIndex = -1;
         private readonly List<Proc<IList>> collectionDelegates = new List<Proc<IList>>();
         private readonly List<Proc<object>> uniqueResultDelegate = new List<Proc<object>>();
         private readonly List<Proc<IList, int>> collectionAndCountDelegate = new List<Proc<IList, int>>();
-
-        private readonly IMultiCriteria multiCriteria;
-
+        readonly List<DetachedCriteria> criteriaList = new List<DetachedCriteria>();
 
         public CriteriaBatch(ISession session)
         {
-            this.multiCriteria = session.CreateMultiCriteria();
+            this.session = session;
+        }
+
+        public CriteriaBatch()
+        {
         }
 
         public CriteriaBatch Add(DetachedCriteria criteria, Order order)
@@ -30,7 +33,7 @@ namespace Rhino.Commons
         public CriteriaBatch Add(DetachedCriteria criteria)
         {
             currentIndex += 1;
-            multiCriteria.Add(criteria);
+            criteriaList.Add(criteria);
             currentCriteria = criteria;
             collectionDelegates.Add(null);
             uniqueResultDelegate.Add(null);
@@ -42,7 +45,7 @@ namespace Rhino.Commons
         {
             uniqueResultDelegate[currentIndex] = delegate(object obj)
             {
-                read((T) obj);
+                read((T)obj);
             };
             return this;
         }
@@ -68,24 +71,40 @@ namespace Rhino.Commons
 
         public void Execute()
         {
+            if (session == null)
+            {
+                throw new InvalidOperationException(
+                    "Session instance variable is null, you must use the Execute(ISession) overload instead");
+            }
+
+            Execute(session);
+        }
+
+        public virtual void Execute(ISession theSession)
+        {
+            IMultiCriteria multiCriteria = theSession.CreateMultiCriteria();
+            foreach (DetachedCriteria detachedCriteria in criteriaList)
+            {
+                multiCriteria.Add(detachedCriteria);
+            }
             IList list = multiCriteria.List();
             int results = list.Count;
             for (int i = 0; i < results; i++)
             {
-                if(collectionDelegates[i]!=null)
+                if (collectionDelegates[i] != null)
                 {
                     collectionDelegates[i]((IList)list[i]);
                 }
                 if (uniqueResultDelegate[i] != null)
                 {
-                    object single = Collection.Single((ICollection) list[i]);
+                    object single = Collection.Single((ICollection)list[i]);
                     uniqueResultDelegate[i](single);
                 }
-                if(collectionAndCountDelegate[i]!=null)
+                if (collectionAndCountDelegate[i] != null)
                 {
                     IList queryList = (IList)list[i];
                     int count = Convert.ToInt32(
-                        Collection.Single((ICollection) list[i + 1])
+                        Collection.Single((ICollection)list[i + 1])
                         );
                     collectionAndCountDelegate[i](queryList, count);
                     i += 1;//not a best practice, I will admit
