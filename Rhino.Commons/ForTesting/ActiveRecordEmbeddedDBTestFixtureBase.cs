5e2732ce18619306e0a8a4f890c2f04f0d2a3cbb
christianacca <christianacca@079b0acf-d9fa-0310-9935-e5ade295c882>
2007-08-20 12:30:37 +0000

    Marked the following classes as obsolete: ActiveRecordEmbeddedDBTestFixtureBase, ActiveRecordInMemoryTestFixtureBase, NHibernateEmbeddedDBTestFixtureBase, NHibernateInMemoryTestFixtureBase
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk/rhino-commons@797 079b0acf-d9fa-0310-9935-e5ade295c882

--- Rhino.Commons/ForTesting/ActiveRecordEmbeddedDBTestFixtureBase.cs
+++ Rhino.Commons/ForTesting/ActiveRecordEmbeddedDBTestFixtureBase.cs
@@ -42,6 +42,7 @@ using Rhino.Commons.Helpers;
 
 namespace Rhino.Commons.ForTesting
 {
+    [Obsolete("Use TestFixtureBase instead")]
 	public class ActiveRecordEmbeddedDBTestFixtureBase
 	{
 		public static string DatabaseFilename = "TempDB.sdf";
