3ceff685c12c3d691cead4e613cc64364142ae8e
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2007-11-15 06:20:48 +0000

    Adding map/reduce to Collection - patch from gabriel Minor relaxations of method signatures in untyped to type converters, allow IEnumerable instead of IList
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@943 079b0acf-d9fa-0310-9935-e5ade295c882

--- rhino-commons/Rhino.Commons/Collection.cs
+++ rhino-commons/Rhino.Commons/Collection.cs
@@ -1,4 +1,5 @@
 ﻿#region license
+
 // Copyright (c) 2005 - 2007 Ayende Rahien (ayende@ayende.com)
 // All rights reserved.
 // 
@@ -24,8 +25,8 @@
 // CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 // THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#endregion
 
+#endregion
 
 using System;
 using System.Collections.Generic;
@@ -34,111 +35,114 @@ using System.Collections;
 
 namespace Rhino.Commons
 {
-    public static class Collection
-    {
-        public static ICollection<T> Containing<T>(params T[] items)
-        {
-            if (items == null) return null;
-            return new List<T>(items);
-        }
+	public static class Collection
+	{
+		public static ICollection<T> Containing<T>(params T[] items)
+		{
+			if (items == null) return null;
+			return new List<T>(items);
+		}
 
-        public static IList<T> ListContaining<T>(params T[] items)
-        {
-            if (items == null) return null;
-            return new List<T>(items);
-        }
+		public static IList<T> ListContaining<T>(params T[] items)
+		{
+			if (items == null) return null;
+			return new List<T>(items);
+		}
 
-        public static T First<T>(ICollection<T> collection)
-        {
-            IList<T> list = collection as IList<T>;
-            if (list != null)
-                return list[0];
-            foreach (T item in collection)
-            {
-                return item;
-            }
-            throw new ElementNotfoundException();
-        }
-        
-        public static T Last<T>(ICollection<T> collection)
-        {
-            IList<T> list = collection as IList<T>;
-            if (list != null)
-                return list[list.Count - 1];
-            T last = default(T);
-            bool set = false;
-            foreach (T item in collection)
-            {
-                last = item;
-                set = true;
-            }
-            if (set)
-                return last;
-            throw new ElementNotfoundException();
-        }
+		public static T First<T>(ICollection<T> collection)
+		{
+			IList<T> list = collection as IList<T>;
+			if (list != null)
+				return list[0];
+			foreach (T item in collection)
+			{
+				return item;
+			}
+			throw new ElementNotfoundException();
+		}
 
-        public static ICollection<T> SelectAll<T>(ICollection<T> collection, Predicate<T> predicate)
-        {
-            return SelectInternal(true, collection, predicate);
-        }
+		public static T Last<T>(ICollection<T> collection)
+		{
+			IList<T> list = collection as IList<T>;
+			if (list != null)
+				return list[list.Count - 1];
+			T last = default(T);
+			bool set = false;
+			foreach (T item in collection)
+			{
+				last = item;
+				set = true;
+			}
+			if (set)
+				return last;
+			throw new ElementNotfoundException();
+		}
 
-        public static T Find<T>(ICollection<T> items, Predicate<T> pred)
-        {
-            foreach (T item in items)
-            {
-                if(pred(item))
-                    return item;
-            }
-            return default(T);
-        }
-        
-        private static ICollection<T> SelectInternal<T>(bool addIfTrue, ICollection<T> collection, Predicate<T> predicate)
-        {
-            ICollection<T> results = new List<T>();
-            foreach (T item in collection)
-            {
-                if(predicate(item))
-                {
-                    if(addIfTrue)
-                        results.Add(item);
-                }
-                else if (addIfTrue == false)
-                {
-                    results.Add(item);
-                }
-            }
-            return results;
-        }
+		public static ICollection<T> SelectAll<T>(ICollection<T> collection, Predicate<T> predicate)
+		{
+			return SelectInternal(true, collection, predicate);
+		}
 
-        public static ICollection<T> SelectAllNot<T>(ICollection<T> collection, Predicate<T> predicate)
-        {
-            return SelectInternal(false, collection, predicate);
-        }
-        
-        public static void ForEach<T>(ICollection<T> collection, Action<T> action)
-        {
-            foreach (T item in collection)
-            {
-                action(item);
-            }
-        }
+		public static T Find<T>(ICollection<T> items, Predicate<T> pred)
+		{
+			foreach (T item in items)
+			{
+				if (pred(item))
+					return item;
+			}
+			return default(T);
+		}
 
-        public static T[] ToArray<T>(object list)
-        {
-            return ToArray<T>((IList) list);
-        }
-        
-        public static T[] ToArray<T>(IList list)
-        {
-            T[] arr = new T[list.Count];
-            list.CopyTo(arr, 0);
-            return arr;
-        }
+		private static ICollection<T> SelectInternal<T>(bool addIfTrue, ICollection<T> collection, Predicate<T> predicate)
+		{
+			ICollection<T> results = new List<T>();
+			foreach (T item in collection)
+			{
+				if (predicate(item))
+				{
+					if (addIfTrue)
+						results.Add(item);
+				}
+				else if (addIfTrue == false)
+				{
+					results.Add(item);
+				}
+			}
+			return results;
+		}
 
-        public static BindingList<T> ToBindingList<T>(IList list)
-        {
-            return new BindingList<T>(ToArray<T>(list));
-        }
+		public static ICollection<T> SelectAllNot<T>(ICollection<T> collection, Predicate<T> predicate)
+		{
+			return SelectInternal(false, collection, predicate);
+		}
+
+		public static void ForEach<T>(ICollection<T> collection, Action<T> action)
+		{
+			foreach (T item in collection)
+			{
+				action(item);
+			}
+		}
+
+		public static T[] ToArray<T>(object list)
+		{
+			return ToArray<T>((IList) list);
+		}
+
+		public static T[] ToArray<T>(IEnumerable list)
+		{
+			List<T> items = new List<T>();
+			foreach (T item in list)
+			{
+				items.Add(item);
+			}
+			return items.ToArray();
+		}
+
+		public static BindingList<T> ToBindingList<T>(IEnumerable list)
+		{
+			return new BindingList<T>(ToArray<T>(list));
+		}
 
 		public static IDictionary<T, ICollection<K>> GroupBy<K, T>(ICollection<K> collection, Converter<K, T> converter)
 		{
@@ -155,29 +159,57 @@ namespace Rhino.Commons
 			return dic;
 		}
 
-        public static object Single(ICollection collection)
-        {
-            if(collection.Count==0)
-                return null;
-            if (collection.Count > 1)
-                throw new InvalidOperationException("Collection does not have exactly one item");
-            IEnumerator enumerator = collection.GetEnumerator();
-            enumerator.MoveNext();
-            return enumerator.Current;
-        }
+		public static object Single(ICollection collection)
+		{
+			if (collection.Count == 0)
+				return null;
+			if (collection.Count > 1)
+				throw new InvalidOperationException("Collection does not have exactly one item");
+			IEnumerator enumerator = collection.GetEnumerator();
+			enumerator.MoveNext();
+			return enumerator.Current;
+		}
 
 
-        public static ICollection<T> ToUniqueCollection<T>(ICollection<T> collection)
-        {
-            if (collection == null) return null;
+		public static ICollection<T> ToUniqueCollection<T>(IEnumerable<T> collection)
+		{
+			if (collection == null) return null;
 
-            List<T> result = new List<T>(collection.Count);
-            foreach (T item in collection)
-            {
-                if (!result.Contains(item))
-                    result.Add(item);
-            }
-            return result;
-        }
-    }
+			List<T> result = new List<T>();
+			foreach (T item in collection)
+			{
+				if (!result.Contains(item))
+					result.Add(item);
+			}
+			return result;
+		}
+
+		public static IList<TResult> Map<TSource, TResult>(
+			IEnumerable<TSource> source,
+			Converter<TSource, TResult> converter)
+		{
+			List<TResult> result = new List<TResult>();
+			if (source != null)
+			{
+				foreach (TSource item in source)
+					result.Add(converter(item));
+			}
+			return result;
+		}
+
+		public delegate TResult Accumelator<TSource, TResult>(TSource source, TResult result);
+
+		public static TResult Reduce<TSource, TResult>(
+			IEnumerable<TSource> source, TResult startValue,
+			Accumelator<TSource, TResult> accumulator)
+		{
+			TResult result = startValue;
+			if (source != null)
+			{
+				foreach (TSource item in source)
+					result = accumulator(item, result);
+			}
+			return result;
+		}
+	}
 }
