a0ae925f1263dc85f718acd1606713bf6d7e53b6
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2007-11-28 14:06:43 +0000

    Fixing broken test.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@964 079b0acf-d9fa-0310-9935-e5ade295c882

--- NHibernate.Query.Generator/NHibernate.Query.Generator.Model/Patient.cs
+++ NHibernate.Query.Generator/NHibernate.Query.Generator.Model/Patient.cs
@@ -4,105 +4,105 @@ using Castle.ActiveRecord;
 
 namespace NHibernate.Query.Generator.Tests.ActiveRecord
 {
-    [ActiveRecord]
-    public class Patient : ActiveRecordBase<Patient>
-    {
-        private long id;
-        private IList<PatientRecord> patientRecords = new List<PatientRecord>();
+	[ActiveRecord]
+	public class Patient : ActiveRecordBase<Patient>
+	{
+		private long id;
+		private IList<PatientRecord> patientRecords = new List<PatientRecord>();
 
-        [PrimaryKey]
-        public virtual long Id
-        {
-            get { return id; }
-            set { id = value; }
-        }
+		[PrimaryKey(Generator = PrimaryKeyType.HiLo)]
+		public virtual long Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
 
-        [HasMany(typeof(PatientRecord), "PatientId", "PatientRecords", Cascade = ManyRelationCascadeEnum.All)]
-        public virtual IList<PatientRecord> PatientRecords
-        {
-            get { return patientRecords; }
-            set { patientRecords = value; }
-        }
+		[HasMany(typeof(PatientRecord), "PatientId", "PatientRecords", Cascade = ManyRelationCascadeEnum.All)]
+		public virtual IList<PatientRecord> PatientRecords
+		{
+			get { return patientRecords; }
+			set { patientRecords = value; }
+		}
 
-        public Patient()
-        {
-        }
+		public Patient()
+		{
+		}
 
-        public Patient(IEnumerable<PatientRecord> patientRecords)
-        {
-            foreach (PatientRecord record in patientRecords)
-            {
-                this.patientRecords.Add(record);
-            }
-        }
-    }
+		public Patient(IEnumerable<PatientRecord> patientRecords)
+		{
+			foreach (PatientRecord record in patientRecords)
+			{
+				this.patientRecords.Add(record);
+			}
+		}
+	}
 
-    [ActiveRecord]
-    public class PatientRecord : ActiveRecordBase<PatientRecord>
-    {
-        private long id;
-        private Name name;
-        private DateTime? birthDate;
+	[ActiveRecord]
+	public class PatientRecord : ActiveRecordBase<PatientRecord>
+	{
+		private long id;
+		private Name name;
+		private DateTime? birthDate;
 
-        [PrimaryKey]
-        public virtual long Id
-        {
-            get { return id; }
-            set { id = value; }
-        }
+		[PrimaryKey]
+		public virtual long Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
 
-        [Nested]
-        public virtual Name Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
+		[Nested]
+		public virtual Name Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
 
-        [Property]
-        public virtual DateTime? BirthDate
-        {
-            get { return birthDate; }
-            set { birthDate = value; }
-        }
+		[Property]
+		public virtual DateTime? BirthDate
+		{
+			get { return birthDate; }
+			set { birthDate = value; }
+		}
 
-        public PatientRecord()
-        {
-        }
+		public PatientRecord()
+		{
+		}
 
-        public PatientRecord(Name name, DateTime? birthDate)
-        {
-            this.name = name;
-            this.birthDate = birthDate;
-        }
-    }
+		public PatientRecord(Name name, DateTime? birthDate)
+		{
+			this.name = name;
+			this.birthDate = birthDate;
+		}
+	}
 
-    public class Name
-    {
-        private string firstName;
-        private string lastName;
+	public class Name
+	{
+		private string firstName;
+		private string lastName;
 
-        [Property]
-        public virtual string FirstName
-        {
-            get { return firstName; }
-            set { firstName = value; }
-        }
+		[Property]
+		public virtual string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
 
-        [Property]
-        public virtual string LastName
-        {
-            get { return lastName; }
-            set { lastName = value; }
-        }
+		[Property]
+		public virtual string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
 
-        public Name()
-        {
-        }
+		public Name()
+		{
+		}
 
-        public Name(string firstName, string lastName)
-        {
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
-    }
+		public Name(string firstName, string lastName)
+		{
+			this.firstName = firstName;
+			this.lastName = lastName;
+		}
+	}
 }
