7ada5ce872cbb56303a4adb532b4cdf7cf097ea1
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2008-02-25 03:11:30 +0000

    Entire build works through nant and all tests are passing. Removing external user spike.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/branches/move-to-nant@1226 079b0acf-d9fa-0310-9935-e5ade295c882

--- rhino-security/Rhino.Security/ChangeSchemaVisitor.cs
+++ rhino-security/Rhino.Security/ChangeSchemaVisitor.cs
@@ -1,36 +1,36 @@
 namespace Rhino.Security
 {
-    using Castle.ActiveRecord.Framework.Internal;
+	using Castle.ActiveRecord.Framework.Internal;
 
-    internal class ChangeSchemaVisitor : AbstractDepthFirstVisitor
-    {
-        private readonly SecurityTableStructure tableStructure;
+	internal class ChangeSchemaVisitor : AbstractDepthFirstVisitor
+	{
+		private readonly SecurityTableStructure tableStructure;
 
-        public ChangeSchemaVisitor(SecurityTableStructure tableStructure)
-        {
-            this.tableStructure = tableStructure;
-        }
+		public ChangeSchemaVisitor(SecurityTableStructure tableStructure)
+		{
+			this.tableStructure = tableStructure;
+		}
 
-        public override void VisitModel(ActiveRecordModel model)
-        {
-            if (tableStructure == SecurityTableStructure.Schema)
-                model.ActiveRecordAtt.Schema = "security";
-            else
-                model.ActiveRecordAtt.Table = "security_" + model.ActiveRecordAtt.Table;
+		public override void VisitModel(ActiveRecordModel model)
+		{
+			if (tableStructure == SecurityTableStructure.Schema)
+				model.ActiveRecordAtt.Schema = "security";
+			else if (model.ActiveRecordAtt.Table.StartsWith("security_") == false)
+				model.ActiveRecordAtt.Table = "security_" + model.ActiveRecordAtt.Table;
 
-            base.VisitModel(model);
-        }
+			base.VisitModel(model);
+		}
 
-        public override void VisitHasAndBelongsToMany(HasAndBelongsToManyModel model)
-        {
-            if (tableStructure == SecurityTableStructure.Schema)
-            {
-                model.HasManyAtt.Schema = "security";
-            }
-            else
-            {
-                model.HasManyAtt.Table = "security_" + model.HasManyAtt.Table;
-            }
-        }
-    }
+		public override void VisitHasAndBelongsToMany(HasAndBelongsToManyModel model)
+		{
+			if (tableStructure == SecurityTableStructure.Schema)
+			{
+				model.HasManyAtt.Schema = "security";
+			}
+			else if (model.HasManyAtt.Table.StartsWith("security_") == false)
+			{
+				model.HasManyAtt.Table = "security_" + model.HasManyAtt.Table;
+			}
+		}
+	}
 }
--- rhino-security/Rhino.Security/Security.cs
+++ rhino-security/Rhino.Security/Security.cs
@@ -1,102 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Castle.ActiveRecord;
+using Castle.ActiveRecord.Framework;
+using Castle.ActiveRecord.Framework.Internal;
+using Castle.Core.Logging;
+using Rhino.Commons;
+
 namespace Rhino.Security
 {
-    using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.Reflection;
-    using Castle.ActiveRecord;
-    using Castle.ActiveRecord.Framework;
-    using Castle.ActiveRecord.Framework.Internal;
-    using Castle.Core.Logging;
-    using Commons;
+	/// <summary>
+	/// This class allows to configure the security system
+	/// </summary>
+	public class Security
+	{
+		private static readonly MethodInfo getSecurityKeyMethod = typeof (Security).GetMethod(
+			"GetSecurityKeyPropertyInternal", BindingFlags.NonPublic | BindingFlags.Static);
 
-    /// <summary>
-    /// This class allows to configure the security system
-    /// </summary>
-    public class Security
-    {
+		private static readonly Dictionary<Type, Rhino.Commons.Func<string>> GetSecurityKeyPropertyCache =
+			new Dictionary<Type, Rhino.Commons.Func<string>>();
 
-        /// <summary>
-        /// Prepares to change all internal reference in the security system
-        /// from IUser to the user implementation of the project
-        /// </summary>
-        public static void PrepareForActiveRecordInitialization<TUser>()
-            where TUser : IUser
-        {
-                PrepareForActiveRecordInitialization<TUser>(SecurityTableStructure.Schema);
-        }
+		/// <summary>
+		/// Gets the logger for the security system.
+		/// </summary>
+		/// <value>The logger.</value>
+		public static ILogger Logger
+		{
+			get { return IoC.TryResolve<ILogger>(new NullLogger()); }
+		}
 
-        /// <summary>
-        /// Prepares to change all internal reference in the security system
-        /// from IUser to the user implementation of the project
-        /// </summary>
-        /// <typeparam name="TUser">The type of the user.</typeparam>
-        /// <param name="tableStructure">The table structure.</param>
-        public static void PrepareForActiveRecordInitialization<TUser>(SecurityTableStructure tableStructure)
-            where TUser : IUser
-        {
-            ActiveRecordStarter.ModelsValidated += delegate(ActiveRecordModelCollection models, IConfigurationSource source)
-                {
-                    foreach (ActiveRecordModel model in models)
-                    {
-                        if (model.Type.Assembly != typeof(IUser).Assembly)
-                            continue;
-                        model.Accept(new AddCachingVisitor());
-                        model.Accept(new ReplaceUserVisitor(typeof(TUser)));
-                        model.Accept(new ChangeSchemaVisitor(tableStructure));
-                    }
-                };
-        }
+		/// <summary>
+		/// Prepares to change all internal reference in the security system
+		/// from IUser to the user implementation of the project
+		/// </summary>
+		public static void PrepareForActiveRecordInitialization<TUser>()
+			where TUser : IUser
+		{
+			PrepareForActiveRecordInitialization<TUser>(SecurityTableStructure.Schema);
+		}
 
-        /// <summary>
-        /// Prepares to change all internal reference in the security system
-        /// from IUser to the user implementation of the project when the user
-        /// is not an entity in the model, but an external cocnept
-        /// </summary>
-        /// <typeparam name="TExternalUser">The type of the external user.</typeparam>
-        /// <param name="tableStructure">The table structure.</param>
-        public static void PrepareForActiveRecordInitializationWithExternalUser<TExternalUser>(SecurityTableStructure tableStructure)
-            where TExternalUser:IExternalUser, new()
-        {
-            ActiveRecordStarter.ModelsValidated += delegate(ActiveRecordModelCollection models, IConfigurationSource source)
-                  {
-                      foreach (ActiveRecordModel model in models)
-                      {
-                          if (model.Type.Assembly != typeof(IUser).Assembly)
-                              continue;
-                          model.Accept(new AddCachingVisitor());
-                          model.Accept(new ReplaceUserWithExternalVisitor<TExternalUser>());
-                          model.Accept(new ChangeSchemaVisitor(tableStructure));
-                      }
-                  };
-        }
+		/// <summary>
+		/// Prepares to change all internal reference in the security system
+		/// from IUser to the user implementation of the project
+		/// </summary>
+		/// <typeparam name="TUser">The type of the user.</typeparam>
+		/// <param name="tableStructure">The table structure.</param>
+		public static void PrepareForActiveRecordInitialization<TUser>(SecurityTableStructure tableStructure)
+			where TUser : IUser
+		{
+			ModelsCreatedDelegate validated = null;
+			validated = delegate(ActiveRecordModelCollection models, IConfigurationSource source)
+		    {
+				ActiveRecordStarter.ModelsValidated -= validated;
+      			foreach (ActiveRecordModel model in models)
+      			{
+      				if (model.Type.Assembly != typeof (IUser).Assembly)
+      					continue;
+      				model.Accept(new AddCachingVisitor());
+      				model.Accept(new ReplaceUserVisitor(typeof (TUser)));
+      				model.Accept(new ChangeSchemaVisitor(tableStructure));
+           		}
+		    };
+			ActiveRecordStarter.ModelsValidated += validated;
+		}
 
-        /// <summary>
-        /// Gets the logger for the security system.
-        /// </summary>
-        /// <value>The logger.</value>
-        public static ILogger Logger
-        {
-            get
-            {
-                return IoC.TryResolve<ILogger>(new NullLogger());
-
-            }
-        }
-
-        /// <summary>
-        /// Extracts the key from the specified entity.
-        /// </summary>
-        /// <typeparam name="TEntity">The type of the entity.</typeparam>
-        /// <param name="entity">The entity.</param>
-        /// <returns></returns>
-        public static Guid ExtractKey<TEntity>(TEntity entity)
-            where TEntity : class
-        {
-            Guard.Against<ArgumentNullException>(entity == null, "Entity cannot be null");
-            IEntityInformationExtractor<TEntity> extractor = IoC.Resolve<IEntityInformationExtractor<TEntity>>();
-            return extractor.GetSecurityKeyFor(entity);
-        }
+		/// <summary>
+		/// Extracts the key from the specified entity.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of the entity.</typeparam>
+		/// <param name="entity">The entity.</param>
+		/// <returns></returns>
+		public static Guid ExtractKey<TEntity>(TEntity entity)
+			where TEntity : class
+		{
+			Guard.Against<ArgumentNullException>(entity == null, "Entity cannot be null");
+			var extractor = IoC.Resolve<IEntityInformationExtractor<TEntity>>();
+			return extractor.GetSecurityKeyFor(entity);
+		}
 
 		/// <summary>
 		/// Gets a human readable description for the specified entity
@@ -104,38 +84,35 @@ namespace Rhino.Security
 		/// <typeparam name="TEntity">The type of the entity.</typeparam>
 		/// <param name="entity">The entity.</param>
 		/// <returns></returns>
-		 public static string GetDescription<TEntity>(TEntity entity) where TEntity : class
+		public static string GetDescription<TEntity>(TEntity entity) where TEntity : class
 		{
-            IEntityInformationExtractor<TEntity> extractor = IoC.Resolve<IEntityInformationExtractor<TEntity>>();
-            return extractor.GetDescription(ExtractKey(entity));
+			var extractor = IoC.Resolve<IEntityInformationExtractor<TEntity>>();
+			return extractor.GetDescription(ExtractKey(entity));
 		}
 
-        /// <summary>
-        /// Gets the security key property for the specified entity type
-        /// </summary>
-        /// <param name="entityType">Type of the entity.</param>
-        /// <returns></returns>
-        public static string GetSecurityKeyProperty(Type entityType)
-        {
-            lock(GetSecurityKeyPropertyCache)
-            {
-                Rhino.Commons.Func<string> func;
-                if (GetSecurityKeyPropertyCache.TryGetValue(entityType, out func))
-                    return func();
-                func = (Rhino.Commons.Func<string>)
-                    Delegate.CreateDelegate(typeof (Rhino.Commons.Func<string>), 
-                        getSecurityKeyMethod.MakeGenericMethod(entityType));
-                GetSecurityKeyPropertyCache[entityType] = func;
-                return func();
-            }
-        }
+		/// <summary>
+		/// Gets the security key property for the specified entity type
+		/// </summary>
+		/// <param name="entityType">Type of the entity.</param>
+		/// <returns></returns>
+		public static string GetSecurityKeyProperty(Type entityType)
+		{
+			lock (GetSecurityKeyPropertyCache)
+			{
+				Rhino.Commons.Func<string> func;
+				if (GetSecurityKeyPropertyCache.TryGetValue(entityType, out func))
+					return func();
+				func = (Rhino.Commons.Func<string>)
+					   Delegate.CreateDelegate(typeof(Rhino.Commons.Func<string>),
+				                               getSecurityKeyMethod.MakeGenericMethod(entityType));
+				GetSecurityKeyPropertyCache[entityType] = func;
+				return func();
+			}
+		}
 
-        internal static string GetSecurityKeyPropertyInternal<TEntity>()
-        {
-            return IoC.Resolve<IEntityInformationExtractor<TEntity>>().SecurityKeyPropertyName;
-        }
-
-        private readonly static Dictionary<Type, Rhino.Commons.Func<string>> GetSecurityKeyPropertyCache = new Dictionary<Type, Rhino.Commons.Func<string>>();
-        private readonly static MethodInfo getSecurityKeyMethod = typeof(Security).GetMethod("GetSecurityKeyPropertyInternal", BindingFlags.NonPublic | BindingFlags.Static);
-    }
+		internal static string GetSecurityKeyPropertyInternal<TEntity>()
+		{
+			return IoC.Resolve<IEntityInformationExtractor<TEntity>>().SecurityKeyPropertyName;
+		}
+	}
 }
