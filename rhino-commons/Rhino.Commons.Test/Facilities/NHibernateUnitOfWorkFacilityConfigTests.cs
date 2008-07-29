b9a2dbef4c0e9baa06526821792ef7974f2d2ec3
jmeckley <jmeckley@079b0acf-d9fa-0310-9935-e5ade295c882>
2008-07-29 20:42:25 +0000

    added assembly overloads so client can pass assembly instead of assembly name to config. add guard.against statements to prevent client from pass invalid arguments to config. seperated config tests into BDD fixtures Added the ability for the client to define how to register entities. (default is return false) removed the flag to determine if entities should be registered.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@1587 079b0acf-d9fa-0310-9935-e5ade295c882

--- rhino-commons/Rhino.Commons.Test/Facilities/NHibernateUnitOfWorkFacilityConfigTests.cs
+++ rhino-commons/Rhino.Commons.Test/Facilities/NHibernateUnitOfWorkFacilityConfigTests.cs
@@ -6,16 +6,16 @@ using System;
 namespace Rhino.Commons.Test.Facilities
 {
     [TestFixture]
-    public class NHibernateUnitOfWorkFacilityConfigTests
+    public class When_instantiating_a_NHibernateUnitOfWorkFacilityConfig
     {
         [Test]
-        public void A_new_instance_should_have_zero_assemblies()
+        public void should_have_zero_assemblies_with_empty_ctor()
         {
             Assert.IsEmpty(new NHibernateUnitOfWorkFacilityConfig().Assemblies);
         }
 
         [Test]
-        public void A_new_instance_can_include_one_assembly()
+        public void should_include_one_assembly_when_loaded_by_name()
         {
             Assembly assembly = GetType().Assembly;
             Assembly[] assemblies = new NHibernateUnitOfWorkFacilityConfig(assembly.FullName).Assemblies;
@@ -25,88 +25,185 @@ namespace Rhino.Commons.Test.Facilities
         }
 
         [Test]
-        public void A_new_instance_should_have_zero_entities()
-        {
-            Assert.IsEmpty(new NHibernateUnitOfWorkFacilityConfig().Entities);
-        }
-
-        [Test]
-        public void A_new_instance_should_not_register_entities_to_repositories()
-        {
-            Assert.IsFalse(new NHibernateUnitOfWorkFacilityConfig().ShouldRegisterEntitiesToRepository);
-        }
-
-        [Test]
-        public void A_new_instance_should_use_the_default_nhibernate_config_file()
-        {
-            Assert.AreEqual("hibernate.cfg.xml", new NHibernateUnitOfWorkFacilityConfig().NHibernateConfigurationFile);
-        }
-
-        [Test]
-        public void Should_be_able_to_add_additional_assembly()
+        public void should_include_one_assembly()
         {
             Assembly assembly = GetType().Assembly;
-            Assembly[] assemblies = new NHibernateUnitOfWorkFacilityConfig(assembly.FullName)
-                .AddAssembly(typeof(NHibernateUnitOfWorkFacilityConfig).GetType().Assembly.FullName)
-                .Assemblies;
-
-            Assert.AreEqual(2, assemblies.Length);
-        }
-
-        [Test]
-        public void Should_include_exactly_one_instance_of_an_assembly()
-        {
-            Assembly assembly = GetType().Assembly;
-            Assembly[] assemblies = new NHibernateUnitOfWorkFacilityConfig(assembly.FullName)
-                .AddAssembly(assembly.FullName)
-                .Assemblies;
+            Assembly[] assemblies = new NHibernateUnitOfWorkFacilityConfig(assembly).Assemblies;
 
             Assert.AreEqual(1, assemblies.Length);
             Assert.AreEqual(assembly, assemblies[0]);
         }
 
+        [Test]
+        public void should_have_zero_entities()
+        {
+            Assert.IsEmpty(new NHibernateUnitOfWorkFacilityConfig().Entities);
+        }
 
         [Test]
-        public void Should_be_able_to_add_an_entity()
+        public void should_have_a_default_predicate_to_register_no_entities()
+        {
+            Assert.IsFalse(new NHibernateUnitOfWorkFacilityConfig().IsCandidateForRepository.Invoke(typeof(object)));
+        }
+
+        [Test]
+        public void should_use_the_default_nhibernate_config_file()
+        {
+            Assert.AreEqual("hibernate.cfg.xml", new NHibernateUnitOfWorkFacilityConfig().NHibernateConfigurationFile);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void will_throw_an_excpetion_for_a_null_string()
+        {
+            string nullstring = null;
+            new NHibernateUnitOfWorkFacilityConfig(nullstring);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void will_throw_an_excpetion_for_an_empty_string()
+        {
+            new NHibernateUnitOfWorkFacilityConfig(string.Empty);
+        }
+    }
+
+    [TestFixture]
+    public class When_adding_assemblies_to_NHibernateUnitOfWorkFacilityConfig
+    {
+        Assembly thisAssembly, thatAssembly;
+
+        [SetUp]
+        public void Before_each_test()
+        {
+            thisAssembly = GetType().Assembly;
+            thatAssembly = typeof(NHibernateUnitOfWorkFacilityConfig).GetType().Assembly;
+        }
+
+        [Test]
+        public void should_add_unique_assemblies_by_name()
+        {
+            Assembly[] assemblies = CreateSUT()
+                .AddAssembly(thisAssembly.FullName)
+                .AddAssembly(thisAssembly.FullName)
+                .AddAssembly(thatAssembly.FullName)
+                .Assemblies;
+
+            Assert.AreEqual(2, assemblies.Length);
+            Assert.In(thisAssembly, assemblies);
+            Assert.In(thatAssembly, assemblies);
+        }
+
+        [Test]
+        public void should_add_unique_assemblies()
+        {
+            Assembly[] assemblies = CreateSUT()
+                .AddAssembly(thisAssembly)
+                .AddAssembly(thisAssembly)
+                .AddAssembly(thatAssembly)
+                .Assemblies;
+
+            Assert.AreEqual(2, assemblies.Length);
+            Assert.In(thisAssembly, assemblies);
+            Assert.In(thatAssembly, assemblies);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void will_throw_an_excpetion_for_null()
+        {
+            string nullstring = null;
+            CreateSUT().AddAssembly(nullstring);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void will_throw_an_excpetion_for_an_empty_string()
+        {
+            CreateSUT().AddAssembly(string.Empty);
+        }
+
+        private NHibernateUnitOfWorkFacilityConfig CreateSUT()
+        {
+            return new NHibernateUnitOfWorkFacilityConfig();
+        }
+    }
+
+    [TestFixture]
+    public class When_adding_entities_to_NHibernateUnitOfWorkFacilityConfig
+    {
+        [Test]
+        public void should_add_unique_types()
         {
             Type _int = typeof(int);
             Type _string = typeof(string);
-            Type[] entities = new NHibernateUnitOfWorkFacilityConfig()
+            Type[] entities = CreateSUT()
+                .AddEntity(_int)
                 .AddEntity(_int)
                 .AddEntity(_string)
                 .Entities;
 
             Assert.AreEqual(2, entities.Length);
-            CollectionAssert.Contains(entities, _int);
-            CollectionAssert.Contains(entities, _string);
+            Assert.In(_int, entities);
+            Assert.In(_string, entities);
         }
 
-        [Test]
-        public void Should_include_exactly_one_instance_of_an_entity()
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void will_throw_an_excpetion_for_null()
         {
-            Type _int = typeof(int);
-            Type[] entities = new NHibernateUnitOfWorkFacilityConfig()
-                .AddEntity(_int)
-                .AddEntity(_int)
-                .Entities;
-
-            Assert.AreEqual(1, entities.Length);
-            Assert.AreEqual(_int, entities[0]);
+            CreateSUT().AddEntity(null);
         }
 
-        [Test]
-        public void Should_be_able_to_set_flag_to_register_entities()
+        private NHibernateUnitOfWorkFacilityConfig CreateSUT()
         {
-            NHibernateUnitOfWorkFacilityConfig config = new NHibernateUnitOfWorkFacilityConfig().RegisterEntities(true);
-            Assert.IsTrue(config.ShouldRegisterEntitiesToRepository);
+            return new NHibernateUnitOfWorkFacilityConfig();
         }
+    }
 
+    [TestFixture]
+    public class When_changing_the_NH_config_for_NHibernateUnitOfWorkFacilityConfig
+    {
         [Test]
-        public void Should_be_able_to_change_nhibernate_configuration_file()
+        public void should_override_default_value()
         {
             string file = "foo";
-            NHibernateUnitOfWorkFacilityConfig config = new NHibernateUnitOfWorkFacilityConfig().NHibernateConfiguration(file);
-            Assert.AreEqual(file, config.NHibernateConfigurationFile);
+            Assert.AreEqual(file, CreateSUT().NHibernateConfiguration(file).NHibernateConfigurationFile);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void will_throw_an_excpetion_for_null_string()
+        {
+            CreateSUT().NHibernateConfiguration(null);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void will_throw_an_excpetion_for_an_empty_string()
+        {
+            CreateSUT().NHibernateConfiguration(string.Empty);
+        }
+
+        private NHibernateUnitOfWorkFacilityConfig CreateSUT()
+        {
+            return new NHibernateUnitOfWorkFacilityConfig();
+        }
+    }
+
+    [TestFixture]
+    public class When_changing_the_entity_registry_to_NHibernateUnitOfWorkFacilityConfig
+    {
+        [Test]
+        public void should_override_the_default_predicate()
+        {
+            Predicate<Type> typeIsAlwaysTrue = delegate(Type t) { return true; };
+            NHibernateUnitOfWorkFacilityConfig config = CreateSUT().RegisterEntitiesWhere(typeIsAlwaysTrue);
+            Assert.AreEqual(typeIsAlwaysTrue, config.IsCandidateForRepository);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentNullException))]
+        public void will_throw_an_excpetion_for_null()
+        {
+            CreateSUT().RegisterEntitiesWhere(null);
+        }
+
+        private NHibernateUnitOfWorkFacilityConfig CreateSUT()
+        {
+            return new NHibernateUnitOfWorkFacilityConfig();
         }
     }
 }
--- rhino-commons/Rhino.Commons.Test/Facilities/NHibernateUnitOfWorkFacilityTests.cs
+++ rhino-commons/Rhino.Commons.Test/Facilities/NHibernateUnitOfWorkFacilityTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Reflection;
 using Rhino.Commons.Facilities;
 using Rhino.Commons.Test.Repository;
 using Castle.Windsor;
@@ -9,13 +11,11 @@ namespace Rhino.Commons.Test.Facilities
     public class NHibernateUnitOfWorkFacilityTests
     {
         private static readonly string facilityKey = "nh.uow.facility";
-        private string thisAssembly;
 
         [SetUp]
         public void Setup()
         {
             IoC.Initialize(new WindsorContainer());
-            thisAssembly = GetType().Assembly.FullName;
         }
 
         [TearDown]
@@ -27,36 +27,40 @@ namespace Rhino.Commons.Test.Facilities
         [Test]
         public void Should_register_NHibernateUnitOfWorkFactory_to_the_container()
         {
-            IoC.Container.AddFacility(facilityKey, new NHibernateUnitOfWorkFacility(new NHibernateUnitOfWorkFacilityConfig(thisAssembly)));
+            IoC.Container.AddFacility(facilityKey, new NHibernateUnitOfWorkFacility(BuildFacilityConfiguration()));
             Assert.IsInstanceOfType(typeof(NHibernateUnitOfWorkFactory), IoC.Resolve<IUnitOfWorkFactory>());
-        }
-
-        [Test]
-        public void Should_register_NHRepository_to_the_container()
-        {
-            IoC.Container.AddFacility(facilityKey, new NHibernateUnitOfWorkFacility(new NHibernateUnitOfWorkFacilityConfig(thisAssembly)));
-            Assert.IsNotNull(Repository<Parent>.Create());
         }
 
         [Test]
         public void Should_configure_session_factory_with_entities()
         {
-            IoC.Container.AddFacility(facilityKey, new NHibernateUnitOfWorkFacility(
-                new NHibernateUnitOfWorkFacilityConfig(thisAssembly)
-                .NHibernateConfiguration("facilities\\hibernate2.cfg.xml")));
+            IoC.Container.AddFacility(facilityKey, new NHibernateUnitOfWorkFacility(BuildFacilityConfiguration()));
 
             using (UnitOfWork.Start())
-                Assert.IsNotNull(UnitOfWork.GetCurrentSessionFor(typeof (Parent)).SessionFactory.GetClassMetadata(typeof(Parent)));
+                Assert.IsNotNull(UnitOfWork.GetCurrentSessionFor(typeof(Parent)).SessionFactory.GetClassMetadata(typeof(Parent)));
+        }
+
+        [Test]
+        public void Should_register_NHRepository_to_the_container()
+        {
+            IoC.Container.AddFacility(facilityKey, new NHibernateUnitOfWorkFacility(BuildFacilityConfiguration()));
+            Assert.IsNotNull(Repository<Parent>.Create());
         }
 
         [Test]
         public void Should_register_entities_to_the_repository()
         {
-            IoC.Container.AddFacility(facilityKey, new NHibernateUnitOfWorkFacility(
-                new NHibernateUnitOfWorkFacilityConfig(thisAssembly)
-                .RegisterEntities(true)
-                .NHibernateConfiguration("facilities\\hibernate2.cfg.xml")));
+            Predicate<Type> entityInThisAssembly = delegate(Type t) { return t.Assembly == GetType().Assembly; };
+            NHibernateUnitOfWorkFacilityConfig config = BuildFacilityConfiguration().RegisterEntitiesWhere(entityInThisAssembly);
+
+            IoC.Container.AddFacility(facilityKey, new NHibernateUnitOfWorkFacility(config));
             Assert.IsNotNull(Repository<IParent>.Create());
+        }
+
+        private NHibernateUnitOfWorkFacilityConfig BuildFacilityConfiguration()
+        {
+            return new NHibernateUnitOfWorkFacilityConfig(GetType().Assembly)
+                .NHibernateConfiguration("facilities\\hibernate2.cfg.xml");
         }
     }
 }
