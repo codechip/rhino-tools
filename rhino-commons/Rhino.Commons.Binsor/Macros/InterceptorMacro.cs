51e18feb4a8e938d1c89cc3e91d87a2c4be2c7f8
cneuwirt <cneuwirt@079b0acf-d9fa-0310-9935-e5ade295c882>
2008-08-25 20:48:05 +0000

    Applied patch from Tim Scott to add interceptor support to Binsor.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@1616 079b0acf-d9fa-0310-9935-e5ade295c882

---
+++ rhino-commons/Rhino.Commons.Binsor/Macros/InterceptorMacro.cs
@@ -0,0 +1,19 @@
+namespace Rhino.Commons.Binsor.Macros
+{
+	using System;
+	using Boo.Lang.Compiler.Ast;
+
+	[CLSCompliant(false)]
+    public class InterceptorMacro : BaseBinsorExtensionMacro<InterceptorExtension>
+    {
+        public InterceptorMacro() : base("interceptor", false, new[] { "component" })
+        {
+        }
+
+        protected override bool ExpandExtension(ref MethodInvocationExpression extension, MacroStatement macro, MacroStatement parent, ref Statement expansion)
+        {
+            extension.Arguments.Add(macro.Arguments[0]);
+            return true;
+        }
+    }
+}
--- rhino-commons/Rhino.Commons.Test/Binsor/UsingAllTypes.boo
+++ rhino-commons/Rhino.Commons.Test/Binsor/UsingAllTypes.boo
@@ -7,7 +7,8 @@ for type in AllTypesWithAttribute of ControllerAttribute("Rhino.Commons.Test"):
 	component type
 	
 for type in AllTypes("Rhino.Commons.Test").WhereNamespaceEq("Rhino.Commons.Test.Binsor"):
-	component type
+	if not Kernel.HasComponent(type):  
+		component type
 
 for type in AllTypes("Rhino.Commons.NHibernate") \
 	.Where({ t as System.Type | t.Name.Contains("NHRepository") }):
