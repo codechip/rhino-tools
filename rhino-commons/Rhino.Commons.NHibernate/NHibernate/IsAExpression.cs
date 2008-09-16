b1411c7105259fbfc80f4f989537aad539a586d9
cneuwirt <cneuwirt@079b0acf-d9fa-0310-9935-e5ade295c882>
2008-09-16 16:38:39 +0000

    Fixed namespace error.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@1653 079b0acf-d9fa-0310-9935-e5ade295c882

--- rhino-commons/Rhino.Commons.NHibernate/NHibernate/IsAExpression.cs
+++ rhino-commons/Rhino.Commons.NHibernate/NHibernate/IsAExpression.cs
@@ -26,7 +26,7 @@
 // THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion
 
-namespace Rhino.Commmons
+namespace Rhino.Commons
 {
 	using System;
 	using System.Collections.Generic;
