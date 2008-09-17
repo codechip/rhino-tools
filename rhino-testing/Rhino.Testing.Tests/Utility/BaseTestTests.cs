de65da5b13334354a896e6982e9db6e9b62ae094
jmeckley <jmeckley@079b0acf-d9fa-0310-9935-e5ade295c882>
2008-09-17 15:25:26 +0000

    create BaseTest as a helper object for Playback/Record testing.
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@1656 079b0acf-d9fa-0310-9935-e5ade295c882

---
+++ rhino-testing/Rhino.Testing.Tests/Utility/BaseTestTests.cs
@@ -0,0 +1,61 @@
+using MbUnit.Framework;
+using Rhino.Testing.Utility;
+using Rhino.Testing.Tests.AutoMocking;
+using Rhino.Mocks;
+using System;
+
+namespace Rhino.Testing.Tests.Utility
+{
+    [TestFixture]
+    public class BaseTestTests : BaseTest
+    {
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            InitializeContainer();
+        }
+
+        [TearDown]
+        public void AfterEachTest()
+        {
+            DestoryContainer();
+        }
+
+        [Test]
+        public void ShouldCreateSystemUnderTest()
+        {
+            ComponentBeingConfigured sut = CreateSUT<ComponentBeingConfigured>();
+            Assert.IsNotNull(sut);
+        }
+
+        [Test]
+        public void ShouldRecordAndPlayback()
+        {
+            ComponentBeingConfigured sut = CreateSUT<ComponentBeingConfigured>();
+            using (Record)
+            {
+                Expect.Call(Mock<ICollectionOfServices>().SomethingToDispose).Return(Mock<IDisposable>());
+                Expect.Call(delegate { Mock<IDisposable>().Dispose(); });
+            }
+
+            using (Playback)
+            {
+                sut.RunDispose();
+            }
+            
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void ShouldReplayAllAndReturnPlayback()
+        {
+            ComponentBeingConfigured sut = CreateSUT<ComponentBeingConfigured>();
+
+            using (PlaybackOnly)
+            {
+                sut.RunDispose();
+            }
+
+        }
+    }
+}
---
+++ rhino-testing/Rhino.Testing/Utility/BaseTest.cs
@@ -0,0 +1,63 @@
+using Rhino.Mocks;
+using Rhino.Testing.AutoMocking;
+using System;
+
+namespace Rhino.Testing.Utility
+{
+    public abstract class BaseTest
+    {
+        private MockRepository mockery;
+        private AutoMockingContainer container;
+
+        protected AutoMockingContainer Container
+        {
+            get { return container; }
+        }
+
+        protected void InitializeContainer()
+        {
+            mockery = new MockRepository();
+            container = new AutoMockingContainer(mockery);
+            container.Initialize();
+        }
+
+        protected void DestoryContainer()
+        {
+            container.Dispose();
+        }
+
+        protected IDisposable Record
+        {
+            get { return mockery.Record(); }
+        }
+
+        protected IDisposable Playback
+        {
+            get { return mockery.Playback(); }
+        }
+
+        protected IDisposable PlaybackOnly
+        {
+            get 
+            {
+                using (Record) { }
+                return mockery.Playback(); 
+            }
+        }
+
+        protected T Mock<T>() where T : class
+        {
+            return container.Get<T>();
+        }
+
+        protected Item CreateSUT<Item>()
+        {
+            return container.Create<Item>();
+        }
+
+        protected void ProvideAnImplementationOf<Interface>(object instance)
+        {
+            container.AddService(typeof(Interface), instance);
+        }
+    }
+}
