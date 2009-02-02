8942274e6eb615590fb9accf2372d34477878308
ayenderahien <ayenderahien@079b0acf-d9fa-0310-9935-e5ade295c882>
2009-02-02 11:29:04 +0000

    Applying patch from Matt Burton - better service install experience. ignoring generated files
    git-svn-id: https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk@1952 079b0acf-d9fa-0310-9935-e5ade295c882

--- rhino-service.bus/Rhino.ServiceBus.Host/Actions/InstallAction.cs
+++ rhino-service.bus/Rhino.ServiceBus.Host/Actions/InstallAction.cs
@@ -15,14 +15,20 @@ namespace Rhino.ServiceBus.Host.Actions
                 Description = options.Name,
                 Context = new InstallContext()
             };
-            installer.Install(new Hashtable());
+			
+			installer.Context.Parameters.Add("assemblypath", GetType().Assembly.Location);
+			
+			installer.Install(new Hashtable());
+
             using (var system = Registry.LocalMachine.OpenSubKey("System"))
             using (var currentControlSet = system.OpenSubKey("CurrentControlSet"))
             using (var services = currentControlSet.OpenSubKey("Services"))
             using (var service = services.OpenSubKey(installer.ServiceName, true))
             {
                 var path = (string)service.GetValue("ImagePath");
-                
+
+				options.Action = Action.Server;
+
                 service.SetValue("ImagePath", path + options);
             }
         }
